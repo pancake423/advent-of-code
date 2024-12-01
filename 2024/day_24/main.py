4041bd5d865ef6bb47f7e8430419efad20333f93
pancake423 <dibbybucklenuggets@gmail.com>
2024-11-30 22:15:54 -0500

    created template, initialized empty days

---
+++ 2024/day_24/main.py
@@ -0,0 +1,23 @@
+import math
+
+example_data = None
+data = None
+
+with open("example.txt") as f:
+    example_data = f.readlines()
+with open("input.txt") as f:
+    data = f.readlines()
+
+
+def part_one(data):
+    pass
+
+def part_two(data):
+    pass
+
+
+if __name__ == "__main__":
+    print("Part one (example input):", part_one(example_data))
+    print("Part one (full input):", part_one(data))
+    print("Part two (example input):", part_two(example_data))
+    print("Part two (full input):", part_two(data))
---
+++ init.py
@@ -0,0 +1,28 @@
+# initialize a year's worth of advent of code puzzles:
+# 1. create a directory with the current year
+# 2. create a directory for each day 1-25
+# 3. fill each with a copy of template.py
+# 4. make a blank text file example.txt and input.txt
+import os
+import shutil
+
+TEMPLATE = "template.py"
+SCRIPT_NAME = "main.py"
+TXT_FILES = ["example.txt", "input.txt"]
+
+year = input("year=")
+try:
+    os.mkdir(year)
+except FileExistsError:
+    print(f"Folder '{year}' already exists. Days that already have a folder will be ignored.")
+
+for i in range(25):
+    path = os.path.join(year, "day_" + str(i+1))
+    try:
+        os.mkdir(path)
+        shutil.copy(TEMPLATE, path)
+        os.rename(os.path.join(path, TEMPLATE), os.path.join(path, SCRIPT_NAME))
+        for filename in TXT_FILES:
+            open(os.path.join(path, filename), "w").close()
+    except FileExistsError:
+        print(f"ignoring existing folder for day {i+1}...")
