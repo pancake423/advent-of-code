dd650fce7d6dad012aa50b0b47edbefa8bf749af
pancake423 <dibbybucklenuggets@gmail.com>
2024-12-14 21:23:40 -0500

    day 11 solution
    I'm happy that I found the dictionary/count trick to stop the branching from going crazy

--- 2024/day_11/solution.py
+++ 2024/day_11/solution.py
@@ -1,16 +1,54 @@
+import time
+
 # ========================= #
 # TESTING CONTROLS
 # set to true or false to
 # control what gets run.
-P1_EXAMPLE = True
-P1_FULL = False
+P1_EXAMPLE = False
+P1_FULL = True
 
 P2_EXAMPLE = False
-P2_FULL = False
+P2_FULL = True
 # ========================= #
 
 def part_one(raw, lines, grid):
-    pass
+    stones = [int(n) for n in lines[0].strip().split(" ")]
+    return dict_solve(stones, 25)
+
+def inc(d, v, amt):
+    if v in d:
+        d[v] += amt
+    else:
+        d[v] = amt
+
+def step_dict(d):
+    out = {}
+    for v in d.keys():
+        if v == 0:
+            inc(out, 1, d[v])
+            continue
+        s = str(v)
+        l = len(s)
+        if l % 2 == 0:
+            inc(out, int(s[:l // 2]), d[v])
+            inc(out, int(s[l // 2:]), d[v])
+            continue
+        inc(out, v*2024, d[v])
+    return out
+
+
+def dict_solve(stones, n):
+    stones_dict = {}
+    for s in stones:
+        if s in stones_dict:
+            stones_dict[s] += 1
+        else:
+            stones_dict[s] = 1
+    for i in range(n):
+        stones_dict = step_dict(stones_dict)
+    return sum(stones_dict.values())
+
 
 def part_two(raw, lines, grid):
-    pass
+    stones = [int(n) for n in lines[0].strip().split(" ")]
+    return dict_solve(stones, 75)
