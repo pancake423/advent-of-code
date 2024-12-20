227dd436a951f03b879984ccbac8f55a8b59fb14
pancake423 <dibbybucklenuggets@gmail.com>
2024-12-20 13:43:08 -0500

    optimized day 18 solution
    using a BFS instead of dijkstra's copied from day 16 made part one near instant. I was also able to reuse the BFS function for
    part 2, making the code significantly shorter.

--- 2024/day_18/solution.py
+++ 2024/day_18/solution.py
@@ -16,41 +16,23 @@ def get(grid, i, j):
         return -1
     return grid[i][j]
 
-def make_graph(memory):
-    graph = {}
-    for i, row in enumerate(memory):
-        for j, n in enumerate(row):
-            if n == 0:
-                candidates = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
-                graph[(i, j)] = [(p, 1) for p in filter(lambda p: get(memory, *p) == 0, candidates)]
-    return graph
-
-def dijkstra(start, graph):
-    def closest_node(nodes):
-        min = inf
-        min_n = ((-1, -1), -1)
-        for node, dist in nodes.items():
-            if dist < min:
-                min = dist
-                min_n = node
-        return min_n, min
-    # shit implementation, really slow but it works
-    visited = {}
-    unvisited = {}
-    for v in graph.keys():
-        unvisited[v] = 0 if v == start else inf
-
-    while True:
-        node, cost = closest_node(unvisited)
-        if cost == inf:
-            break
-        for edge_node, edge_cost in graph[node]:
-            if edge_node in unvisited and unvisited[edge_node] > cost + edge_cost:
-                unvisited[edge_node] = cost + edge_cost
-        visited[node] = cost
-        del unvisited[node]
-
-    return visited
+def shortest_path(memory, goal_pos):
+    # use a BFS to find the shortest path through the memory
+    explored = set([(0, 0)])
+    depth = [0] #queue holding the depth of nodes to explore
+    queue = [(0, 0)] #queue holding the position of nodes to explore
+    while len(queue) > 0:
+        i,j = queue.pop(0)
+        d = depth.pop(0)
+        if (i, j) == goal_pos:
+            return d
+        coords = ((i+1, j), (i, j+1), (i-1, j), (i, j-1))
+        for c in coords:
+            if c not in explored and get(memory, *c) == 0:
+                explored.add(c)
+                queue.append(c)
+                depth.append(d+1)
+    return inf
 
 
 def part_one(raw, lines, grid):
@@ -63,32 +45,7 @@ def part_one(raw, lines, grid):
         x, y = incoming[i]
         memory[x][y] = 1
 
-    graph = make_graph(memory)
-    sssp = dijkstra((0, 0), graph)
-    return sssp[(grid_size-1, grid_size-1)]
-
-# determine if there is any path (doesn't find optimal path)
-# through the given memory
-def is_path(start, goal, memory):
-    # depth first search of memory space looking for any connection
-    # to the goal
-    visited = set()
-    def dfs(i, j, goal):
-        if (i, j) == goal:
-            return True
-        if get(memory, i, j) != 0:
-            return False
-        if (i, j) in visited:
-            return False
-        visited.add((i, j))
-
-        coords = ((i+1, j), (i, j+1), (i-1, j), (i, j-1))
-        for c in coords:
-            if dfs(*c, goal):
-                return True
-        return False
-
-    return dfs(*start, goal)
+    return shortest_path(memory, (grid_size-1, grid_size-1))
 
 # determine if the memory is passable based on the number of bytes corrupted
 def is_blocked(n_corrupted, incoming, grid_size):
@@ -97,7 +54,7 @@ def is_blocked(n_corrupted, incoming, grid_size):
         x, y = incoming[i]
         memory[x][y] = 1
     # draw(memory, incoming[n_corrupted-1])
-    return not is_path((0, 0), (grid_size-1, grid_size-1), memory)
+    return shortest_path(memory, (grid_size-1, grid_size-1)) == inf
 
 # for debugging
 def draw(grid, last_blocked):
