19811fa85ef0e6eb7ba26f0455fc0df2f910a4cd
pancake423 <dibbybucklenuggets@gmail.com>
2025-11-02 12:58:10 -0500

    2022 day 3
    a much more normal solution to make up for the chaos last time.
    sets are insanely goated data structures <3

--- 2022/day_3/solution.py
+++ 2022/day_3/solution.py
@@ -3,14 +3,44 @@
 # set to true or false to
 # control what gets run.
 P1_EXAMPLE = True
-P1_FULL = False
+P1_FULL = True
 
-P2_EXAMPLE = False
-P2_FULL = False
+P2_EXAMPLE = True
+P2_FULL = True
 # ========================= #
 
+def split_line(line):
+    hl = len(line) // 2
+    return [line[:hl], line[hl:]]
+
+def common_items( *args ):
+    common = set(args[0])
+    for arg in args:
+        common &= set(arg)
+    return list(common)
+
+def priority(char):
+    ord_c = ord(char)
+    if ord_c >= 97:
+        return ord_c - ord("a") + 1
+    return ord_c - ord("A") + 27
+
+def group_lines(lines, n):
+    i = 0
+    out = []
+    while i < len(lines):
+        item = []
+        for j in range(n):
+            item.append(lines[i])
+            i += 1
+        out.append(item)
+    return out
+
+
+
 def part_one(raw, lines, grid):
-    pass
+    # split lines in half
+    return sum([priority(common_items( *split_line(line) )[0]) for line in lines])
 
 def part_two(raw, lines, grid):
-    pass
+    return sum([priority(common_items( *group )[0]) for group in group_lines(lines, 3)])
