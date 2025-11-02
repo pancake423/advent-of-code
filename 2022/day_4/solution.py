76d1193c180e72e28c9bcc5cec0742a2c13401f0
pancake423 <dibbybucklenuggets@gmail.com>
2025-11-02 13:25:19 -0500

    2022 day 4
    on a roll now

--- 2022/day_4/solution.py
+++ 2022/day_4/solution.py
@@ -3,14 +3,35 @@
 # set to true or false to
 # control what gets run.
 P1_EXAMPLE = True
-P1_FULL = False
+P1_FULL = True
 
-P2_EXAMPLE = False
-P2_FULL = False
+P2_EXAMPLE = True
+P2_FULL = True
 # ========================= #
 
+def parse_input(lines):
+    return [[[int(n) for n in p.split("-")] for p in line.split(",")] for line in lines]
+
+def contained(p1, p2):
+    return p1[0] >= p2[0] and p1[1] <= p2[1]
+
+def contained_both(p1, p2):
+    return 1 if contained(p1, p2) or contained(p2, p1) else 0
+
+def point_within(p, range):
+    return p >= range[0] and p <= range[1]
+
+def overlaps(p1, p2):
+    tests = [
+        (p1[0], p2),
+        (p1[1], p2),
+        (p2[0], p1),
+        (p2[1], p1)
+    ]
+    return 1 if any(point_within( *test ) for test in tests) else 0
+
 def part_one(raw, lines, grid):
-    pass
+    return sum(contained_both( *pair ) for pair in parse_input(lines))
 
 def part_two(raw, lines, grid):
-    pass
+    return sum(overlaps( *pair ) for pair in parse_input(lines))
