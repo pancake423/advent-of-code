5c99f05fb7174191f5c2ad97cd2393fb95b19e4a
pancake423 <dibbybucklenuggets@gmail.com>
2025-11-05 15:21:17 -0500

    day 6 2022

--- 2022/day_6/solution.py
+++ 2022/day_6/solution.py
@@ -3,14 +3,29 @@
 # set to true or false to
 # control what gets run.
 P1_EXAMPLE = True
-P1_FULL = False
+P1_FULL = True
 
-P2_EXAMPLE = False
-P2_FULL = False
+P2_EXAMPLE = True
+P2_FULL = True
 # ========================= #
 
+# returns true iff every character of s in the substring [i, i+n) is unique
+def unique(s, i, n = 4):
+    return len(set(s[i:i+n])) == n
+
+# returns the first position, p, in string s where the substring [0, p] contains n unique characters in a row
+def first_unique_range(s, n):
+    for i in range(len(s) - n):
+        if unique(s, i, n):
+            return i + n
+    return -1
+
+# just for fun, because solving an entire AoC puzzle in one line is pretty satisfying
+def first_unique_range_oneliner(s, n):
+    return min(i if len(set(s[i:i+n])) == n else len(s) for i in range(len(s) - n))
+
 def part_one(raw, lines, grid):
-    pass
+    return first_unique_range(raw, 4)
 
 def part_two(raw, lines, grid):
-    pass
+    return first_unique_range(raw, 14)
