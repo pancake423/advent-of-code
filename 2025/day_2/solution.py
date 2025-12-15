00b4503ed143ec5cf48d140b21f33e12c24e5b81
William Jackson <william@wxj.me>
2025-12-15 09:38:10 -0500

    2025 day 2 (fast version)
    who could resist optimizing here?
    sped up the solution by a factor of about 1000 by generating viable candidates
    instead of checking every number in the range for repeated sequences.
    (~1s to <1ms for part 1, ~5s to 6ms for part 2 on my laptop)

--- 2025/day_2/solution.py
+++ 2025/day_2/solution.py
@@ -2,12 +2,14 @@
 # TESTING CONTROLS
 # set to true or false to
 # control what gets run.
-P1_EXAMPLE = False
-P1_FULL = False
+P1_EXAMPLE = True
+P1_FULL = True
 
 P2_EXAMPLE = True
 P2_FULL = True
 # ========================= #
+
+from math import ceil
 
 
 def parse_ranges(lines):
@@ -35,19 +37,64 @@ def repeated_pattern(n):
     return False
 
 
-def part_one(raw, lines, grid):
-    sum = 0
+def first_half(n):
+    s = str(n)
+    return int(s[: ceil(len(s) / 2)])
+
+
+def solve_fast_p1(lines):
+    total = 0
     for r in parse_ranges(lines):
-        for i in range(r[0], r[1] + 1):
-            if repeated_digits(i):
-                sum += i
-    return sum
+        low = first_half(r[0])
+        high = first_half(r[1])
+        if low > high:
+            low //= 10
+        for i in range(low, high + 1):
+            n = int(str(i) * 2)
+            if r[0] <= n <= r[1]:
+                total += n
+    return total
+
+
+def solve_fast_p2(lines):
+    total = 0
+    for r in parse_ranges(lines):
+        matches = set()
+        max_l = len(str(r[1]))
+        min_l = len(str(r[0]))
+        low = first_half(r[0])
+        high = first_half(r[1])
+        if low > high:
+            high *= 10
+        for n in range(low, high + 1):
+            s = str(n)
+            for i in range(1, len(s) + 1):
+                pattern = s[:i]
+                cmp = int(pattern * (max_l // i))
+                if max_l > 1 and r[0] <= cmp <= r[1]:
+                    matches.add(cmp)
+                cmp = int(pattern * (min_l // i))
+                if min_l > 1 and r[0] <= cmp <= r[1]:
+                    matches.add(cmp)
+        total += sum(matches)
+    return total
+
+
+def part_one(raw, lines, grid):
+    # sum = 0
+    # for r in parse_ranges(lines):
+    #     for i in range(r[0], r[1] + 1):
+    #         if repeated_digits(i):
+    #             sum += i
+    # return sum
+    return solve_fast_p1(lines)
 
 
 def part_two(raw, lines, grid):
-    sum = 0
-    for r in parse_ranges(lines):
-        for i in range(r[0], r[1] + 1):
-            if repeated_pattern(i):
-                sum += i
-    return sum
+    # sum = 0
+    # for r in parse_ranges(lines):
+    #     for i in range(r[0], r[1] + 1):
+    #         if repeated_pattern(i):
+    #             sum += i
+    # return sum
+    return solve_fast_p2(lines)
