1f5328f6afb7dde8872a5265cee0e7983d75cf22
William Jackson <william@wxj.me>
2025-12-15 09:59:46 -0500

    2025 day 3
    I was able to guess what the twist for part two would be, which is always satisfying.
    Very clean solve, I'm happy I was able to figure out the optimal solution from the start.

--- 2025/day_3/solution.py
+++ 2025/day_3/solution.py
@@ -3,14 +3,36 @@
 # set to true or false to
 # control what gets run.
 P1_EXAMPLE = True
-P1_FULL = False
+P1_FULL = True
 
-P2_EXAMPLE = False
-P2_FULL = False
+P2_EXAMPLE = True
+P2_FULL = True
 # ========================= #
 
+
+def max_sublist(l, n):
+    # where l is a list of digits,
+    # and n is the number of digits in the sublist
+    if n == 1:
+        return [max(l)]
+    idx = 0
+    max_v = 0
+    for i, d in enumerate(l[: -n + 1]):
+        if d > max_v:
+            idx = i
+            max_v = d
+
+    return [max_v] + max_sublist(l[idx + 1 :], n - 1)
+
+
+# convert a list of digits to an integer
+def to_int(list):
+    return int("".join([str(n) for n in list]))
+
+
 def part_one(raw, lines, grid):
-    pass
+    return sum(to_int(max_sublist([int(n) for n in line], 2)) for line in grid)
+
 
 def part_two(raw, lines, grid):
-    pass
+    return sum(to_int(max_sublist([int(n) for n in line], 12)) for line in grid)
