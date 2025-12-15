4aca0ed08e8cdfb6e6978827ad35386f812928dc
William Jackson <william@wxj.me>
2025-12-15 08:51:14 -0500

    2025 Day 2
    late to the party this time around but oh well.. finals are tough
    
    this is a very brute-forcey solution. It should be possible to look at the patterns of starting digits
    and identify where repeated sequences of digits are within each range rather than checking each number in the range.

--- 2025/day_2/solution.py
+++ 2025/day_2/solution.py
@@ -2,15 +2,52 @@
 # TESTING CONTROLS
 # set to true or false to
 # control what gets run.
-P1_EXAMPLE = True
+P1_EXAMPLE = False
 P1_FULL = False
 
-P2_EXAMPLE = False
-P2_FULL = False
+P2_EXAMPLE = True
+P2_FULL = True
 # ========================= #
 
+
+def parse_ranges(lines):
+    return [[int(n) for n in range.split("-")] for range in "".join(lines).split(",")]
+
+
+def repeated_digits(n):
+    s = str(n)
+    l = len(s) // 2
+    return s[:l] == s[l:]
+
+
+def repeated_pattern(n):
+    s = str(n)
+    max_l = len(s) // 2
+    for p in range(1, max_l + 1):
+        cmp = s[:p]
+        match = True
+        for i in range(0, len(s), p):
+            if s[i : i + p] != cmp:
+                match = False
+                break
+        if match:
+            return True
+    return False
+
+
 def part_one(raw, lines, grid):
-    pass
+    sum = 0
+    for r in parse_ranges(lines):
+        for i in range(r[0], r[1] + 1):
+            if repeated_digits(i):
+                sum += i
+    return sum
+
 
 def part_two(raw, lines, grid):
-    pass
+    sum = 0
+    for r in parse_ranges(lines):
+        for i in range(r[0], r[1] + 1):
+            if repeated_pattern(i):
+                sum += i
+    return sum
